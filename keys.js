6cbbe671699ad0ba77e0418b4d711a8fd637f6df
Kimberley Hood <KimberleyHood@kimberleys-mbp.attlocal.net>
2017-05-12 07:04:11 -0500

    update

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '0nL3EfzMjcA2NwAbsRnPSBGr2',
+  consumer_secret: 'KhJXPFMpmvlcZ5SCdTJRXdB5UnxzvAAECYbluumbp4DSYhNoc0',
+  access_token_key: '744723585130860545-6x7DBFXAUR03YfjzrqMBcNu6EirBIcU',
+  access_token_secret: 'UKWBXwzcTTJBhpfTwZka0tZLZ2fhuwxC3KPBjCZxWMHmD',
+}
